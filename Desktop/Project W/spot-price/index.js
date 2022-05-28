6bf4985ddc2a62b51d2f673eb176e7683129a001
MK Work <mk.hcj2@gmail.com>
2022-05-28 15:50:44 +0900

    function added

--- Desktop/Project W/spot-price/index.js
+++ Desktop/Project W/spot-price/index.js
@@ -8,7 +8,42 @@ over 120cm -> +jpy 10,000/box or
 over 70kgn -> +jpy 10,500/box
 */
 function getPrice() {
-    let totalWeight = document.getElementById("boxWeightA").value
+    let totalWeight = document.getElementsByClassName("boxWeight")[0].value;
     let totalPrice = document.getElementById("answer");
     totalPrice.innerHTML = totalWeight;
 }
+/*const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
+console.log(sum); // 6*/
+priceTotal = (a) => {
+    return a * 1200 * 1.32 + (a * 20) ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addBox() {
+let tag = document.createElement("p");
+let text = document.createTextNode("Tutorix is the best e-learning platform");
+tag.appendChild(text);
+let element = document.getElementById("new");
+element.appendChild(tag);}
